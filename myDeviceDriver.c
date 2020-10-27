1b71eec1cc9c92f50e169517d5f3ae31ba351786
Yoshimasa Obayashi <obayashi.yoshimasa@seek-the-truth.org>
2020-10-27 21:36:24 +0900

    06_01

--- myDeviceDriver.c
+++ myDeviceDriver.c
@@ -9,18 +9,9 @@
 #include <linux/slab.h>
 #include <asm/current.h>
 #include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <asm/io.h>
-
-/* ペリフェラルレジスタの物理アドレス(BCM2835の仕様書より) */
-#define REG_ADDR_BASE        (0x3F000000)		/* bcm_host_get_peripheral_address()の方がbetter */
-#define REG_ADDR_GPIO_BASE   (REG_ADDR_BASE + 0x00200000)
-#define REG_ADDR_GPIO_GPFSEL_0     0x0000
-#define REG_ADDR_GPIO_OUTPUT_SET_0 0x001C
-#define REG_ADDR_GPIO_OUTPUT_CLR_0 0x0028
-#define REG_ADDR_GPIO_LEVEL_0      0x0034
-
-#define REG(addr) (*((volatile u32 *)(addr)))
-#define DUMP_REG(addr) printk("%08X\n", REG(addr));
+#include "myDeviceDriver.h"
 
 /*** このデバイスに関する情報 ***/
 MODULE_LICENSE("Dual BSD/GPL");
@@ -31,77 +22,71 @@ static unsigned int mydevice_major;			/* このデバイスドライバのメジ
 static struct cdev mydevice_cdev;			/* キャラクタデバイスのオブジェクト */
 static struct class *mydevice_class = NULL;	/* デバイスドライバのクラスオブジェクト */
 
+/* ioctlテスト用に値を保持する変数 */
+static struct mydevice_values stored_values;
+
+/* ioctl時に呼ばれる関数 */
+static long mydevice_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	printk("mydevice_ioctl\n");
+	
+	switch (cmd) {
+	case MYDEVICE_SET_VALUES:
+		printk("MYDEVICE_SET_VALUES\n");
+		if (copy_from_user(&stored_values, (void __user *)arg, sizeof(stored_values))) {
+			return -EFAULT;
+		}
+		break;
+	case MYDEVICE_GET_VALUES:
+		printk("MYDEVICE_GET_VALUES\n");
+		if (copy_to_user((void __user *)arg, &stored_values, sizeof(stored_values))) {
+			return -EFAULT;
+		}
+		break;
+	default:
+		printk(KERN_WARNING "unsupported command %d\n", cmd);
+		return -EFAULT;
+	}
+	return 0;
+}
+
+
 /* open時に呼ばれる関数 */
 static int mydevice_open(struct inode *inode, struct file *file)
 {
-	printk("mydevice_open");
-
-	/* ARM(CPU)から見た物理アドレス → 仮想アドレス(カーネル空間)へのマッピング */
-	void __iomem *address = ioremap_nocache(REG_ADDR_GPIO_BASE + REG_ADDR_GPIO_GPFSEL_0, 4);
-
-	/* GPIO4を出力に設定 */
-	REG(address) = 1 << 12;
-
-	iounmap((void*)address);
-
+	printk("mydevice_open\n");
 	return 0;
 }
 
 /* close時に呼ばれる関数 */
 static int mydevice_close(struct inode *inode, struct file *file)
 {
-	printk("mydevice_close");
+	printk("mydevice_close\n");
 	return 0;
 }
 
 /* read時に呼ばれる関数 */
 static ssize_t mydevice_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
-	printk("mydevice_read");
-
-	/* ARM(CPU)から見た物理アドレス → 仮想アドレス(カーネル空間)へのマッピング */
-	void __iomem *address = ioremap_nocache(REG_ADDR_GPIO_BASE + REG_ADDR_GPIO_LEVEL_0, 4);
-	int val = (REG(address) & (1 << 4)) != 0;	/* GPIO4が0かどうかを0, 1にする */
-
-	/* GPIOの出力値をユーザへ文字として返す */
-	put_user(val + '0', &buf[0]);
-
-	iounmap((void*)address);
-
+	printk("mydevice_read\n");
 	return count;
 }
 
 /* write時に呼ばれる関数 */
 static ssize_t mydevice_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
-	printk("mydevice_write");
-
-	void __iomem *address;
-	char outValue;
-
-	/* ユーザが設定したGPIOへの出力値を取得 */
-	get_user(outValue, &buf[0]);
-
-	/* ARM(CPU)から見た物理アドレス → 仮想アドレス(カーネル空間)へのマッピング */
-	if(outValue == '1') {
-		/* '1'ならSETする */
-		address = ioremap_nocache(REG_ADDR_GPIO_BASE + REG_ADDR_GPIO_OUTPUT_SET_0, 4);
-	} else {
-		/* '0'ならCLRする */
-		address = ioremap_nocache(REG_ADDR_GPIO_BASE + REG_ADDR_GPIO_OUTPUT_CLR_0, 4);
-	}
-	REG(address) = 1 << 4;
-	iounmap((void*)address);
-	
+	printk("mydevice_write\n");
 	return count;
 }
 
 /* 各種システムコールに対応するハンドラテーブル */
-struct file_operations s_mydevice_fops = {
+static struct file_operations mydevice_fops = {
 	.open    = mydevice_open,
 	.release = mydevice_close,
 	.read    = mydevice_read,
 	.write   = mydevice_write,
+	.unlocked_ioctl = mydevice_ioctl,
+	.compat_ioctl   = mydevice_ioctl,	// for 32-bit App
 };
 
 /* ロード(insmod)時に呼ばれる関数 */
@@ -113,22 +98,22 @@ static int mydevice_init(void)
 	int cdev_err = 0;
 	dev_t dev;
 
-	/* 1. 空いているメジャー番号を確保する */
+	/* 空いているメジャー番号を確保する */
 	alloc_ret = alloc_chrdev_region(&dev, MINOR_BASE, MINOR_NUM, DRIVER_NAME);
 	if (alloc_ret != 0) {
 		printk(KERN_ERR  "alloc_chrdev_region = %d\n", alloc_ret);
 		return -1;
 	}
 
-	/* 2. 取得したdev( = メジャー番号 + マイナー番号)からメジャー番号を取得して保持しておく */
+	/* 取得したdev( = メジャー番号 + マイナー番号)からメジャー番号を取得して保持しておく */
 	mydevice_major = MAJOR(dev);
 	dev = MKDEV(mydevice_major, MINOR_BASE);	/* 不要? */
 
-	/* 3. cdev構造体の初期化とシステムコールハンドラテーブルの登録 */
-	cdev_init(&mydevice_cdev, &s_mydevice_fops);
+	/* cdev構造体の初期化とシステムコールハンドラテーブルの登録 */
+	cdev_init(&mydevice_cdev, &mydevice_fops);
 	mydevice_cdev.owner = THIS_MODULE;
 
-	/* 4. このデバイスドライバ(cdev)をカーネルに登録する */
+	/* このデバイスドライバ(cdev)をカーネルに登録する */
 	cdev_err = cdev_add(&mydevice_cdev, dev, MINOR_NUM);
 	if (cdev_err != 0) {
 		printk(KERN_ERR  "cdev_add = %d\n", alloc_ret);
@@ -136,7 +121,7 @@ static int mydevice_init(void)
 		return -1;
 	}
 
-	/* 5. このデバイスのクラス登録をする(/sys/class/mydevice/ を作る) */
+	/* このデバイスのクラス登録をする(/sys/class/mydevice/ を作る) */
 	mydevice_class = class_create(THIS_MODULE, "mydevice");
 	if (IS_ERR(mydevice_class)) {
 		printk(KERN_ERR  "class_create\n");
@@ -145,7 +130,7 @@ static int mydevice_init(void)
 		return -1;
 	}
 
-	/* 6. /sys/class/mydevice/mydevice* を作る */
+	/* /sys/class/mydevice/mydevice* を作る */
 	for (int minor = MINOR_BASE; minor < MINOR_BASE + MINOR_NUM; minor++) {
 		device_create(mydevice_class, NULL, MKDEV(mydevice_major, minor), NULL, "mydevice%d", minor);
 	}
@@ -160,18 +145,18 @@ static void mydevice_exit(void)
 
 	dev_t dev = MKDEV(mydevice_major, MINOR_BASE);
 	
-	/* 7. /sys/class/mydevice/mydevice* を削除する */
+	/* /sys/class/mydevice/mydevice* を削除する */
 	for (int minor = MINOR_BASE; minor < MINOR_BASE + MINOR_NUM; minor++) {
 		device_destroy(mydevice_class, MKDEV(mydevice_major, minor));
 	}
 
-	/* 8. このデバイスのクラス登録を取り除く(/sys/class/mydevice/を削除する) */
+	/* このデバイスのクラス登録を取り除く(/sys/class/mydevice/を削除する) */
 	class_destroy(mydevice_class);
 
-	/* 9. このデバイスドライバ(cdev)をカーネルから取り除く */
+	/* このデバイスドライバ(cdev)をカーネルから取り除く */
 	cdev_del(&mydevice_cdev);
 
-	/* 10. このデバイスドライバで使用していたメジャー番号の登録を取り除く */
+	/* このデバイスドライバで使用していたメジャー番号の登録を取り除く */
 	unregister_chrdev_region(dev, MINOR_NUM);
 }
 
